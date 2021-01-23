b9fa59b7544dd866982045cf9e9688fa79506a21
Davydau Aliaksey <davidoff.aleksei@gmail.com>
2021-01-24 00:16:07 +0300

    feat: add script for installing dependency from number to length of indicator line in progress__bars

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
-
+// const counters = querySelectorAll('.progress__bar__header-num');
+// const lines = querySelectorAll('.progress__bar__meter-indicate')
+// console.log(lines);
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
 });
@@ -9,3 +11,9 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.progress__bar__header-num');
+const lines = document.querySelectorAll('.progress__bar__meter-indicate');
+counters.forEach((item,i)=>{
+	lines[i].style.width=item.innerHTML;
+})
